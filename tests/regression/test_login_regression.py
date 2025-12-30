ebc3801cae4ebe3cc776eca1d1c2cf5182630af1
Sabrina Oroz <sabrinaoroz87@gmail.com>
2025-12-30 14:04:12 -0300

    Fix feature file paths for regression tests (CI compatible)

--- tests/regression/test_login_regression.py
+++ tests/regression/test_login_regression.py
@@ -1,5 +1,11 @@
 # Ese archivo solo registra escenarios (BDD PURO), los datos se encuentran en los feature
 
+#from pathlib import Path                               # Reemplazo de ruta por paths relativos. Fumciona en todos los OS
 from pytest_bdd import scenarios
 
-scenarios("C:\\Users\\sabri\\PycharmProjects\\PythonProject\\bdd\\features\\login.feature")
+
+
+scenarios("../../bdd/features/login.feature")
+
+#FEATURES_DIR = Path(__file__).resolve().parents[2] / "bdd" / "features"
+#scenarios(FEATURES_DIR / "login.feature")
