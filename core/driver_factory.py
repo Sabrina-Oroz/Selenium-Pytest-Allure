e0dada7049226a3cb340bcf4f05ab55899d7e2b4
Sabrina Oroz <sabrinaoroz87@gmail.com>
2026-01-04 13:16:00 -0300

    Add GitHub Actions workflow for UI tests

--- core/driver_factory.py
+++ core/driver_factory.py
@@ -4,26 +4,23 @@
 
 
 from selenium import webdriver                                   # permite controlar el navegador
-from selenium.webdriver.chrome.service import Service            # especifica dónde está ubicado el driver
 from selenium.webdriver.chrome.options import Options            # configura chrome antes de iniciarlo
 from webdriver_manager.chrome import ChromeDriverManager         # descarga el driver del navegador automáticamente
 import os                                                        # módulo de python para interactuar con el os
-from datetime import datetime                                    # módulo de python para obtener marcas de tiempo
+
 
 
 def create_driver():
     chrome_options = Options()
 
-    # Opciones generales antes de abrir chrome
+    # Opciones base previo al abrir el navegador
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--blink-settings=imagesEnabled=false")
 
-    # Control por variable de entorno
     headless = os.getenv("HEADLESS", "false").lower() == "true"
 
     if headless:
-        # === FLAGS OBLIGATORIOS EN CI ===
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
@@ -32,7 +29,6 @@ def create_driver():
     else:
         chrome_options.add_argument("--start-maximized")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
+    #  Selenium Manager (NO webdriver-manager)
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
